2319b77f4d7ca1735b16ce4ebe5e18fad394f6d5
gkozub <glib.kozub@gmail.com>
2018-01-18 10:28:38 +0200

    BurgerIngredient -> add prop types

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import PropTypes from 'prop-types';
 
 import classes from './BurgerIngredient.css';
 
@@ -6,7 +7,7 @@ class BurgerIngredient extends Component {
   render() {
     let ingredient = null;
 
-    switch (props.type) {
+    switch (this.props.type) {
       case ('bread-bottom'):
         ingredient = <div className={classes.BreadBottom}></div>;
         break;
@@ -37,5 +38,9 @@ class BurgerIngredient extends Component {
     return ingredient;
   }
 }
+
+BurgerIngredient.propTypes = {
+  type: PropTypes.string.isRequired
+};
 
 export default BurgerIngredient;
