193a2fe72855d4b476aa9d0532c1946f9bcdf43a
gkozub <glib.kozub@gmail.com>
2018-01-18 10:20:30 +0200

    BurgerIngredient -> add ingredient variants

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,3 +1,39 @@
-const burgerIngredient = (props) => (
-  <div></div>
-);
+import React from 'react';
+
+import clases from './BurgerIngredient.css';
+
+const burgerIngredient = (props) => {
+  let ingredient = null;
+
+  switch (props.type) {
+    case ('bread-bottom'):
+      ingredient = <div className={clases.BreadBottom}></div>;
+      break;
+    case ('bread-top'):
+      ingredient = (
+        <div className={clases.BreadTop}>
+          <div className={clases.Seeds1}></div>
+          <div className={clases.Seeds2}></div>
+        </div>
+      );
+      break;
+    case ('meat'):
+      ingredient = <div className={clases.Meat}></div>;
+      break;
+    case ('cheese'):
+      ingredient = <div className={clases.Cheese}></div>;
+      break;
+    case ('salad'):
+      ingredient = <div className={clases.Salad}></div>;
+      break;
+    case ('bacon'):
+      ingredient = <div className={clases.Bacon}></div>;
+      break;
+    default:
+      ingredient = null;
+  }
+
+  return ingredient;
+};
+
+export default burgerIngredient;
