edf8a3aa1f7b243cfd5713b9471f398b1037f6dd
gkozub <glib.kozub@gmail.com>
2018-01-18 10:24:45 +0200

    BurgerIngredient -> fix typo

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -1,33 +1,33 @@
 import React from 'react';
 
-import clases from './BurgerIngredient.css';
+import classes from './BurgerIngredient.css';
 
 const burgerIngredient = (props) => {
   let ingredient = null;
 
   switch (props.type) {
     case ('bread-bottom'):
-      ingredient = <div className={clases.BreadBottom}></div>;
+      ingredient = <div className={classes.BreadBottom}></div>;
       break;
     case ('bread-top'):
       ingredient = (
-        <div className={clases.BreadTop}>
-          <div className={clases.Seeds1}></div>
-          <div className={clases.Seeds2}></div>
+        <div className={classes.BreadTop}>
+          <div className={classes.Seeds1}></div>
+          <div className={classes.Seeds2}></div>
         </div>
       );
       break;
     case ('meat'):
-      ingredient = <div className={clases.Meat}></div>;
+      ingredient = <div className={classes.Meat}></div>;
       break;
     case ('cheese'):
-      ingredient = <div className={clases.Cheese}></div>;
+      ingredient = <div className={classes.Cheese}></div>;
       break;
     case ('salad'):
-      ingredient = <div className={clases.Salad}></div>;
+      ingredient = <div className={classes.Salad}></div>;
       break;
     case ('bacon'):
-      ingredient = <div className={clases.Bacon}></div>;
+      ingredient = <div className={classes.Bacon}></div>;
       break;
     default:
       ingredient = null;
